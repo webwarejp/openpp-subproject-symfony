995a71a6ab224329dbec2b2c5487bae5a687642f
Yoshihiro Taniguchi <taniguchi@webware.co.jp>
2015-09-01 22:50:55 -0400

    set up fosuserbundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            // USER
+            new FOS\UserBundle\FOSUserBundle(),
+            //OAUTH
+            //new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
